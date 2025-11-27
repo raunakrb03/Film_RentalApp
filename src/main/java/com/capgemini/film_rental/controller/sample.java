333339aefdd85b82d454a63fcf26f855981cb425
Sai Naga Sri Akshaya <pathivada-siva.sai-naga-sri-akshaya@capgemini.com>
2025-11-27 14:43:10 +0530

    change in sample

--- src/main/java/com/capgemini/film_rental/controller/sample.java
+++ src/main/java/com/capgemini/film_rental/controller/sample.java
@@ -1,4 +1,7 @@
 package com.capgemini.film_rental.controller;
 
-public class sample {
+public class sample{
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
 }

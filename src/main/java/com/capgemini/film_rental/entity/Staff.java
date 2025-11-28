97dbff60fa1856bbfcfe278a19a59895b3344cb7
raubatra <raunak.batra@capgemini.com>
2025-11-28 14:59:51 +0530

    Resolved Staff and Store

--- src/main/java/com/capgemini/film_rental/entity/Staff.java
+++ src/main/java/com/capgemini/film_rental/entity/Staff.java
@@ -1,59 +1,235 @@
 package com.capgemini.film_rental.entity;
 
-import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.List;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.Lob;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "staff")
 public class Staff {
 
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "staff_id")
+    @Column(name = "staff_id",columnDefinition = "TINYINT UNSIGNED")
+    // @JsonIgnore
     private int staffId;
 
-    @Column(name = "first_name", length = 45, nullable = false)
+    @Column(name = "first_name", nullable = false, length = 45)
     private String firstName;
 
-    @Column(name = "last_name", length = 45, nullable = false)
+    @Column(name = "last_name", nullable = false, length = 45)
     private String lastName;
 
     @ManyToOne
-    @JoinColumn(name = "address_id", nullable = false)
+    @JoinColumn(name = "address_id", nullable = false,columnDefinition = "SMALLINT UNSIGNED")
+    @JsonIgnore
     private Address address;
 
-    @Column(name = "email", length = 50)
+    @Lob
+    @Column(name = "picture",columnDefinition = "BLOB",nullable = true)
+    private byte[] picture;
+
+    @Column(name = "email", length = 50,nullable = true)
     private String email;
 
     @ManyToOne
-    @JoinColumn(name = "store_id", nullable = false)
+    @JoinColumn(name = "store_id", nullable = false,columnDefinition = "TINYINT UNSIGNED")
+    @JsonIgnore
     private Store store;
 
-    @Column(name = "active", nullable = false)
-    private Boolean active = true;
+    @Column(name = "active", nullable = false,columnDefinition = "TINYINT(1) DEFAULT 1")
+    private byte active=1;
 
-    @Column(name = "username", length = 16, nullable = false)
+    @Column(name = "username", nullable = false, length = 16)
     private String username;
 
-    @Column(name = "password", length = 40)
+    @Column(name = "password", length = 40,nullable = true)
     private String password;
 
-    @Column(name = "last_update", nullable = false)
+    @Column(name = "last_update", nullable = false,columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
     private LocalDateTime lastUpdate;
 
     @OneToMany(mappedBy = "staff")
-    private Set<Rental> rentals = new HashSet<>();
+    @JsonIgnore
+    @Column(name="payment_id",nullable = false,columnDefinition = "TINYINT UNSIGNED")
+    private List<Payment> payment;
 
-    @OneToMany(mappedBy = "staff")
-    private Set<Payment> payments = new HashSet<>();
-
-    @PrePersist @PreUpdate
-    public void touch() {
-        this.lastUpdate = LocalDateTime.now();
+    public int getStaffId() {
+        return staffId;
     }
 
+    public void setStaffId(int staffId) {
+        this.staffId = staffId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Store getStore() {
+        return store;
+    }
+
+    public void setStore(Store store) {
+        this.store = store;
+    }
+
+    public byte getActive() {
+        return active;
+    }
+
+    public void setActive(byte active) {
+        this.active = active;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDateTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(active, address, email, firstName, lastName, lastUpdate, password, staffId, store,
+                username);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Staff other = (Staff) obj;
+        return active == other.active && Objects.equals(address, other.address) && Objects.equals(email, other.email)
+                && Objects.equals(firstName, other.firstName) && Objects.equals(lastName, other.lastName)
+                && Objects.equals(lastUpdate, other.lastUpdate) && Objects.equals(password, other.password)
+                && Objects.equals(staffId, other.staffId) && Objects.equals(store, other.store)
+                && Objects.equals(username, other.username);
+    }
+
+    @Override
+    public String toString() {
+        return "Staff [staffId=" + staffId + ", firstName=" + firstName + ", lastName=" + lastName + ", address="
+                + address + ", email=" + email + ", store=" + store + ", active=" + active + ", username=" + username
+                + ", password=" + password + ", lastUpdate=" + lastUpdate + "]";
+    }
+    public Staff() {
+        // TODO Auto-generated constructor stub
+    }
+
+    public Staff(int staffId, String firstName, String lastName, Address address, String email, Store store,
+                 byte active, String username, String password, LocalDateTime lastUpdate,List<Payment> payment) {
+        super();
+        this.staffId = staffId;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.email = email;
+        this.store = store;
+        this.active = active;
+        this.username = username;
+        this.password = password;
+        this.lastUpdate = lastUpdate;
+        this.payment=payment;
+    }
+
+    public Staff(int staff_id) {
+        // TODO Auto-generated constructor stub
+    }
+
+    public List<Payment> getPayment() {
+        return payment;
+    }
+
+    public void setPayment(List<Payment> payment) {
+        this.payment = payment;
+    }
+
+
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- src/main/java/com/capgemini/film_rental/entity/Store.java
+++ src/main/java/com/capgemini/film_rental/entity/Store.java
@@ -1,11 +1,24 @@
 package com.capgemini.film_rental.entity;
 
 
-import jakarta.persistence.*;
 
 import java.time.LocalDateTime;
-import java.util.HashSet;
-import java.util.Set;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
 
 @Entity
 @Table(name = "store")
@@ -13,33 +26,115 @@ public class Store {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "store_id")
+    @Column(name = "store_id",columnDefinition = "TINYINT UNSIGNED")
+    //@JsonIgnore
     private int storeId;
 
     @OneToOne
-    @JoinColumn(name = "manager_staff_id", nullable = false)
-    private Staff manager;
+    @JoinColumn(name = "manager_staff_id",columnDefinition = "TINYINT UNSIGNED")
+    private Staff managerStaff;
 
     @ManyToOne
-    @JoinColumn(name = "address_id", nullable = false)
+    @JoinColumn(name = "address_id",columnDefinition = "SMALLINT UNSIGNED")
+    //@JsonIgnore
     private Address address;
 
-    @Column(name = "last_update", nullable = false)
+    @Column(name = "last_update", nullable = false,columnDefinition = "TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP")
     private LocalDateTime lastUpdate;
 
     @OneToMany(mappedBy = "store")
-    private Set<Staff> staff = new HashSet<>();
+    @JsonIgnore
+    private List<Staff> staff;
 
     @OneToMany(mappedBy = "store")
-    private Set<Customer> customers = new HashSet<>();
+    @JsonIgnore
+    private List<Customer> customers;
 
     @OneToMany(mappedBy = "store")
-    private Set<Inventory> inventories = new HashSet<>();
+    @JsonIgnore
+    private List<Inventory> inventories=new ArrayList<>();
 
-    @PrePersist
-    @PreUpdate
-    public void touch() {
-        this.lastUpdate = LocalDateTime.now();
+    public int getStoreId() {
+        return storeId;
     }
 
+    public void setStoreId(int storeId) {
+        this.storeId = storeId;
+    }
+
+    public Staff getManagerStaff() {
+        return managerStaff;
+    }
+
+    public void setManagerStaff(Staff managerStaff) {
+        this.managerStaff = managerStaff;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDateTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public List<Staff> getStaff() {
+        return staff;
+    }
+
+    public void setStaff(List<Staff> staff) {
+        this.staff = staff;
+    }
+
+    public List<Customer> getCustomers() {
+        return customers;
+    }
+
+    public void setCustomers(List<Customer> customers) {
+        this.customers = customers;
+    }
+
+    public List<Inventory> getInventories() {
+        return inventories;
+    }
+
+    public void setInventories(List<Inventory> inventories) {
+        this.inventories = inventories;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(address, customers, inventories, lastUpdate, managerStaff, staff, storeId);
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Store other = (Store) obj;
+        return Objects.equals(address, other.address) && Objects.equals(customers, other.customers)
+                && Objects.equals(inventories, other.inventories) && Objects.equals(lastUpdate, other.lastUpdate)
+                && Objects.equals(managerStaff, other.managerStaff) && Objects.equals(staff, other.staff)
+                && Objects.equals(storeId, other.storeId);
+    }
+
+    @Override
+    public String toString() {
+        return "Store [storeId=" + storeId + ", managerStaff=" + managerStaff + ", address=" + address + ", lastUpdate="
+                + lastUpdate + ", staff=" + staff + ", customers=" + customers + ", inventories=" + inventories + "]";
+    }
+
+    // Getters, setters, equals, hashCode, toString
 }
